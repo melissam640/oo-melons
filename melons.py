6a12bfa5b8b9a2e1419e24574ce9abf2e758ab76
Melissa McGrath <melissam640@gmail.com>
2024-03-07 12:40:19 -0800

    revised get_total per prices updates

--- melons.py
+++ melons.py
@@ -14,8 +14,14 @@ class AbstractMelonOrder:
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = 7.5
         total = (1 + self.tax) * self.qty * base_price
+
+        # if self.qty in InternationalMelonOrder: 
+        #      += 3
+            
+        # if self.qty < 10:
+        #     total += 3
 
         return total
     
@@ -40,6 +46,11 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         super().__init__(species, qty)
         self.country_code = country_code
+
+    def get_total(self):
+        total = super().get_total()
+        if self.qty < 10:
+            return total + 3
 
     def get_country_code(self):
         """Return the country code."""
